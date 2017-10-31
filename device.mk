9655628ca35dc15e83dd000e72847072de16ac0b
Dan Pasanen <dan.pasanen@gmail.com>
2017-10-30 19:13:58 -0500

    mata: Add FPC keylayout for fingerprint reader gestures.
    Change-Id: I3c7c5d14a32b076d105c1e2d104e0285ab49d054

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
     fstab.mata \
     init.mata.rc
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl
+
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
