ed34e6583b90483b4b76b63172ba498c6629986f
Anay Wadhera <awadhera@berkeley.edu>
2020-06-18 17:27:14 -0700

    mata: Set up verity.
    Change-Id: I55780aaec5293dbf5f196e7eefd16f63bd820e54

--- device.mk
+++ device.mk
@@ -37,3 +37,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Shipping API
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Verity
+$(call inherit-product, build/target/product/verity.mk)
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/vendor
